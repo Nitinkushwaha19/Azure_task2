55b7ed8e0dab7ea696a209f85d6f88a5ec295bb3
nitinkushwaha <nitinkushwaha706@outlook.com>
2026-04-08 16:23:26 +0530

    fix outputs

--- task04/outputs.tf
+++ task04/outputs.tf
@@ -7,18 +7,3 @@ output "vm_fqdn" {
   description = "Fully Qualified Domain Name of the virtual machine"
   value       = azurerm_public_ip.publicIP.fqdn
 }
-
-output "ssh_connection_command" {
-  description = "SSH command to connect to the VM"
-  value       = "ssh ${var.admin_username}@${azurerm_public_ip.publicIP.ip_address}"
-}
-
-output "nginx_url" {
-  description = "URL to access Nginx web server"
-  value       = "http://${azurerm_public_ip.publicIP.fqdn}"
-}
-
-output "resource_group_name" {
-  description = "Name of the created resource group"
-  value       = azurerm_resource_group.RG.name
-}
